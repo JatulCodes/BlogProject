acce6fd9f580b01f17ba611248297ed33bda00b6
JatulCodes <69075503+JatulCodes@users.noreply.github.com>
2023-01-29 17:03:22 +0530

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,9 @@
 const mongoose = require ('mongoose');
 const DB = process.env.DATABASE;
 mongoose.connect(DB, {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true
+    useNewUrlParser: true,
+      useUnifiedTopology:true,
+      useCreateIndex: true
 }).then(()=>{
     console.log(`connection successfull`);
 }).catch((err) => console.log({err}));
